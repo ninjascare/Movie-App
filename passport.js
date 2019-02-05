567fa078dedd7f67efa1ea97287585178c217127
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-02-05 18:43:14 -0500

    going in a new direcion for authication

--- passport.js
+++ passport.js
@@ -1,18 +1,26 @@
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "73510111687-45jtde82k9ep5jg07n5pvdvfa1evo9b8.apps.googleusercontent.com",
+      clientSecret: "TzKeZ7a_fXD4h6gLpxpeO1Px",
+      callbackURL: "http://www.example.com/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      console.log("accessToken", accessToken);
+      console.log("refreshToken", refreshToken);
+      console.log("profile", profile);
+
+      User.findOrCreate({ googleId: profile.id }, function(err, user) {
+        return done(err, user);
+      });
+    }
+  )
+);
