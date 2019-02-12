fc11061d16ee231dfaa05fd2bc4332213bde2375
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2019-02-12 14:47:21 -0500

    added some more things

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -19,7 +19,6 @@ class App extends Component {
           >
             Learn React
           </a>
-          
         </header>
       </div>
     );
